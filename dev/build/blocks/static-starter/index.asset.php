69a0153e96af72e712c1dc335282faba0fd6a789
Jamie Reevior <jamie@hirozed.me>
2025-04-04 22:44:17 -0400

    Updated the schema in blocks.json to point to the 5.8 version

--- dev/build/blocks/static-starter/index.asset.php
+++ dev/build/blocks/static-starter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e86866c80f71de450e08');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0996108ee2523c3c867d');
--- dev/build/blocks/use-request-data/index.asset.php
+++ dev/build/blocks/use-request-data/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '746a7d855459abe9423e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '24434dcca5f7ec9a0c49');
